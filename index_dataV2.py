0fd04c81229ac1ed79bef3e99fa695d39b61eeae
orelanthos32 <orelanthos@gmail.com>
2021-04-04 04:05:25 +0800

    Update index_dataV2.py

--- index_dataV2.py
+++ index_dataV2.py
@@ -6,9 +6,8 @@ import numpy as np
 es2=Elasticsearch([{'host':'localhost','port':9200}])
 #load the data
 df = pd.read_csv("stockticker10-tweets(5-3-21).csv")
-#hdf = df.drop_duplicates('IntentDesc')
-#hdf["combine"] = hdf["IntentDesc"] +" "+ hdf["KBResponseText"]
-def index_data(df):
+#hdf = df.drop_duplicates('Text')
+def index_data(df, index):
     for index, row in df.iterrows():
         #print(value)
         print(index)
@@ -17,6 +16,9 @@ def index_data(df):
                 "text": row["Text"]
 
             }
-        es2.index(index = "first", doc_type= "word",id =index,body = e)
-index_data(df)
+        es2.index(index = index, doc_type= "word",id =index,body = e)
+index_data(df, "Twitter")
+Reddit_df = pd.read_csv("MAIN_Reddit_Manual - gme1616872068 (1)")
+index_data(Reddit_df, "Reddit")
+
 print("data indexing complete")
