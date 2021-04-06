907ac83376521e632ed2fbc9bf7e1da644f2f04f
orelanthos32 <orelanthos@gmail.com>
2021-04-07 01:51:57 +0800

    Change#5

--- index_dataV2.py
+++ index_dataV2.py
@@ -5,20 +5,24 @@ import numpy as np
 # Connect to the elastic cluster
 es2=Elasticsearch([{'host':'localhost','port':9200}])
 #load the data
-df = pd.read_csv("stockticker10-tweets(5-3-21).csv")
-#hdf = df.drop_duplicates('Text')
-def index_data(df, index):
+df = pd.read_csv("twitter_dataset.csv")
+hdf = df.drop_duplicates('Text')
+Reddit_df = pd.read_csv("reddit_dataset.csv")
+def index_data(df, asset, offset = 0):
     for index, row in df.iterrows():
         #print(value)
         print(index)
         e = {
                 "intent":row["Query"],
-                "text": row["Text"]
-
+                "text": row["Text"],
+				"sentiment": row["Sentiment"],
+				"subjectivity": row["Subjectivity"],
+				"vader": row["VADER"]
             }
-        es2.index(index = index, doc_type= "word",id =index,body = e)
-index_data(df, "Twitter")
-Reddit_df = pd.read_csv("MAIN_Reddit_Manual - gme1616872068 (1)")
-index_data(Reddit_df, "Reddit")
+        es2.index(index = asset,body = e, id= index + offset)
+fdf= Reddit_df.dropna()
+index_data(Reddit_df, "reddit")
+print("**********")
+index_data(hdf, "twitter",5000)
 
 print("data indexing complete")
