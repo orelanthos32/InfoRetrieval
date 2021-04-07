b565d1cbefda8e60f8a0ba17b340083ac21a47aa
orelanthos32 <orelanthos@gmail.com>
2021-04-07 12:18:18 +0800

    Hopefully LAST

--- index_dataV2.py
+++ index_dataV2.py
@@ -8,21 +8,19 @@ es2=Elasticsearch([{'host':'localhost','port':9200}])
 df = pd.read_csv("twitter_dataset.csv")
 hdf = df.drop_duplicates('Text')
 Reddit_df = pd.read_csv("reddit_dataset.csv")
-def index_data(df, asset, offset = 0):
+def index_data(df, asset):
     for index, row in df.iterrows():
         #print(value)
         print(index)
         e = {
                 "intent":row["Query"],
                 "text": row["Text"],
-				"sentiment": row["Sentiment"],
-				"subjectivity": row["Subjectivity"],
 				"vader": row["VADER"]
             }
-        es2.index(index = asset,body = e, id= index + offset)
+        es2.index(index = asset,body = e)
 fdf= Reddit_df.dropna()
 index_data(Reddit_df, "reddit")
 print("**********")
-index_data(hdf, "twitter",5000)
+index_data(hdf, "twitter")
 
 print("data indexing complete")
